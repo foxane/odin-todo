575aa3c727930d3e68b38d02239c64e8d6068041
foxane <65512187+foxane@users.noreply.github.com>
2024-06-05 14:11:56 +0700

    Added localStorage interface

--- src/js/interface.js
+++ src/js/interface.js
@@ -1,1 +1,18 @@
 // Interface with localstorage
+const data = (() => {
+  const getData = () => {
+    return JSON.parse(localStorage.getItem("fxn-todo"));
+  };
+
+  const setData = (dataObj) => {
+    return localStorage.setItem("fxn-todo", JSON.stringify(dataObj));
+  };
+
+  const clearData = () => {
+    localStorage.removeItem("fxn-todo");
+  };
+
+  return { getData, setData, clearData };
+})();
+
+export default data;
