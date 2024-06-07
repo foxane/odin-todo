bd693bdf57ecec48521230673fd05f14ac923967
foxane <65512187+foxane@users.noreply.github.com>
2024-06-07 11:55:23 +0700

    Created project module

---
+++ src/js/index.js
@@ -0,0 +1,1 @@
+import { Project, Task } from "./project";
---
+++ src/js/project.js
@@ -0,0 +1,40 @@
+export { Project, Task };
+import { format } from "date-fns";
+import { v4 as uuidv4 } from "uuid";
+
+// Project
+class Project {
+  static projectList = [];
+  task = [];
+
+  constructor(name) {
+    this.name = name;
+    Project.projectList.push(this); // Push to project for each new instances
+  }
+
+  // Self delete project from static
+  deleteSelf() {
+    const index = Project.projectList.indexOf(this);
+    Project.projectList.splice(index, 1);
+  }
+
+  // Add and remove task from task arr
+  // Both of this must be called from project instance they are on
+  addTask(newTask) {
+    this.task.push(newTask);
+    newTask.index = this.task.indexOf(newTask);
+  }
+  removeTask(toRemove) {
+    this.task.splice(toRemove.index, 1);
+  }
+}
+
+class Task {
+  constructor(title, desc, dueDate, priority = "low") {
+    this.title = title;
+    this.desc = desc;
+    this.dueDate = format(new Date(dueDate), "E',' d MMM uuuu");
+    this.priority = priority;
+    this.id = uuidv4();
+  }
+}
